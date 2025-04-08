81ea59e9ad4ac867f7114177d88d5b5551f16da7
Gaurav Vishwakarma <gaurav25vishwakarma@gmail.com>
2025-04-09 03:44:55 +0530

    Duplicate Data

---
+++ Pandas/10.Duplicate-Data.py
@@ -0,0 +1,17 @@
+# working with duplicate data  
+#
+import pandas as pd
+df=pd.read_csv('Pandas/data.csv')
+print(df.shape)
+# checking for duplicate values
+print(df.duplicated()) # check for duplicate values
+print(df.duplicated().sum()) # check for duplicate values
+# dropping duplicate values
+df.drop_duplicates(inplace=True) # drop duplicate values
+print(df.shape) # check the shape of the dataframe
+# dropping duplicate values based on a specific column
+df.drop_duplicates(subset=['Name'], inplace=True) # drop duplicate values based on a specific column
+print(df.shape) # check the shape of the dataframe
+# dropping duplicate values based on a specific column and keeping the last value
+df.drop_duplicates(subset=['Name'], keep='last', inplace=True) # drop duplicate values based on a specific column and keeping the last value
+print(df.shape) # check the shape of the dataframe
